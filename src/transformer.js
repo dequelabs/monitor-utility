c091d83ce2c4702546d370c57604707893b6ee47
Tommy Braccia <bracciata@gmail.com>
2023-01-26 19:22:51 -0500

    fix: reduced repeated calls

--- src/transformer.js
+++ src/transformer.js
@@ -10,52 +10,37 @@ module.exports = {
         const pid = result.report.report.id;
         const shareHash = sha1(`${pid}${xor}`);
         const shareUrl = `${result.server}/worldspace/organizationProject/summary/${pid}?share=${shareHash}`;
-        const populateGroupToIssuesCountDict = () => {
-          // Todo make this more effiencent by completing it in one run.
-          // It can be combined with severity counting.
-          let groupingToIssueCountDict = {};
-          groupingToIssueCountDict["ARIA"] = getTotal("Aria");
-          groupingToIssueCountDict["Color"] = getTotal("Color");
-          groupingToIssueCountDict["Forms"] = getTotal("Forms");
-          groupingToIssueCountDict["Keyboard"] = getTotal("Keyboard");
-          groupingToIssueCountDict["Language"] = getTotal("Language");
-          groupingToIssueCountDict["Media"] = getTotal("Media");
-          groupingToIssueCountDict["Name Role Value"] =
-            getTotal("Name Role Value");
-          groupingToIssueCountDict["Parsing"] = getTotal("Parsing");
-          groupingToIssueCountDict["PDF"] = getTotal("PDF");
-          groupingToIssueCountDict["Semantics"] = getTotal("Semantics");
-          groupingToIssueCountDict["Sensory And Visual Cues"] = getTotal(
-            "Sensory And Visual Cues"
-          );
-          groupingToIssueCountDict["Structure"] = getTotal("Structure");
-          groupingToIssueCountDict["Tables"] = getTotal("Tables");
-          groupingToIssueCountDict["Text Alternatives"] =
-            getTotal("Text Alternatives");
-          groupingToIssueCountDict["Time"] = getTotal("Time");
-          return groupingToIssueCountDict;
-        };
-        const getTotal = (group = "", priority = "") => {
+
+        const getTotalForGroupsAndPriorities = (dict) => {
           let issues = 0;
           result.report.report.issuelist.forEach((issue) => {
-            if (!group && !priority) {
-              issues = issues + issue.issues;
-            } else if (group && issue.issuegrouping === group) {
-              issues = issues + issue.issues;
-            } else if (priority && issue.priority === priority) {
-              issues = issues + issue.issues;
+            if (issue.issuegrouping) {
+              if (dict[issue.issuegrouping]) {
+                dict[issue.issuegrouping] =
+                  dict[issue.issuegrouping] + issue.issues;
+              } else {
+                dict[issue.issuegrouping] = issue.issues;
+              }
+            }
+            if (issue.priority) {
+              if (dict[issue.priority]) {
+                dict[issue.priority] = dict[issue.priority] + issue.issues;
+              } else {
+                dict[issue.priority] = issue.issues;
+              }
             }
           });
-          return issues;
+          return dict;
         };
-        let groupingToIssueCountDict = populateGroupToIssuesCountDict();
+        let groupingAndPriorityToIssueCountDict =
+          getTotalForGroupsAndPriorities({});
         let transformedResult = {
           Site: result.report.report.name,
           "Shared Report URL": shareUrl,
           "Accessibility Score": result.report.report.score.toString(),
           "Total Pages Tested": result.report.report.totalPages,
           "% of pages with no errors found":
-            ((result.report.report.totalPages - result.report.report.good) /
+            ((result.report.report.good) /
               result.report.report.totalPages) *
             100,
           "Good Pages": result.report.report.good.toString(),
@@ -64,26 +49,31 @@ module.exports = {
           "Critical Pages": result.report.report.critical.toString(),
           "Average # of errors per page":
             result.report.report.issuesPerPage.toString(),
-          "Total Critical Issues": getTotal("", "Critical"),
-          "Total Serious Issues": getTotal("", "Serious"),
-          "Total Moderate Issues": getTotal("", "Moderate"),
-          "Total Minor Issues": getTotal("", "Minor"),
-          ARIA: groupingToIssueCountDict["Aria"],
-          Color: groupingToIssueCountDict["Color"],
-          Forms: groupingToIssueCountDict["Forms"],
-          Keyboard: groupingToIssueCountDict["Keyboard"],
-          Language: groupingToIssueCountDict["Language"],
-          Media: groupingToIssueCountDict["Media"],
-          "Name Role Value": groupingToIssueCountDict["Name Role Value"],
-          Parsing: groupingToIssueCountDict["Parsing"],
-          PDF: groupingToIssueCountDict["PDF"],
-          Semantics: groupingToIssueCountDict["Semantics"],
+          "Total Critical Issues":
+            groupingAndPriorityToIssueCountDict["Critical"],
+          "Total Serious Issues":
+            groupingAndPriorityToIssueCountDict["Serious"],
+          "Total Moderate Issues":
+            groupingAndPriorityToIssueCountDict["Moderate"],
+          "Total Minor Issues": groupingAndPriorityToIssueCountDict["Minor"],
+          ARIA: groupingAndPriorityToIssueCountDict["Aria"],
+          Color: groupingAndPriorityToIssueCountDict["Color"],
+          Forms: groupingAndPriorityToIssueCountDict["Forms"],
+          Keyboard: groupingAndPriorityToIssueCountDict["Keyboard"],
+          Language: groupingAndPriorityToIssueCountDict["Language"],
+          Media: groupingAndPriorityToIssueCountDict["Media"],
+          "Name Role Value":
+            groupingAndPriorityToIssueCountDict["Name Role Value"],
+          Parsing: groupingAndPriorityToIssueCountDict["Parsing"],
+          PDF: groupingAndPriorityToIssueCountDict["PDF"],
+          Semantics: groupingAndPriorityToIssueCountDict["Semantics"],
           "Sensory and Visual Cues":
-            groupingToIssueCountDict["Sensory And Visual Cues"],
-          Structure: groupingToIssueCountDict["Structure"],
-          Tables: groupingToIssueCountDict["Tables"],
-          "Text Alternatives": groupingToIssueCountDict["Text Alternatives"],
-          Time: groupingToIssueCountDict["Time"],
+            groupingAndPriorityToIssueCountDict["Sensory And Visual Cues"],
+          Structure: groupingAndPriorityToIssueCountDict["Structure"],
+          Tables: groupingAndPriorityToIssueCountDict["Tables"],
+          "Text Alternatives":
+            groupingAndPriorityToIssueCountDict["Text Alternatives"],
+          Time: groupingAndPriorityToIssueCountDict["Time"],
         };
         transformedResults.push(transformedResult);
       });
